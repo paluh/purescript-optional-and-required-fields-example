88cbfcbc5ea2ea769ff0c401c06aed9a4a53e448
Tomasz Rybarczyk <paluho@gmail.com>
2020-03-15 14:06:00 +0100

    Provide additional example which captures required fields directly

--- src/Main.purs
+++ src/Main.purs
@@ -8,11 +8,11 @@ import Global.Unsafe (unsafeStringify)
 import Prim.Row (class Union)
 import Type.Row (type (+))
 
-type OptionalFields = ( opt1 ∷ Boolean, opt2 ∷ Int )
+type OptionalFields r = ( opt1 ∷ Boolean, opt2 ∷ Int | r )
 
 stringify
   ∷ ∀ given missing
-  . Union given missing OptionalFields
+  . Union given missing (OptionalFields + ())
   ⇒ { | given }
   → String
 stringify r =
@@ -30,7 +30,7 @@ type RequiredFields r = ( req1 ∷ Number, req2 ∷ String | r )
 stringify'
   ∷ ∀ given optionalMissing optionalPresent
   . Union (RequiredFields + ()) optionalPresent given
-  ⇒ Union given optionalMissing (RequiredFields + OptionalFields)
+  ⇒ Union given optionalMissing (RequiredFields + OptionalFields + ())
   ⇒ { | given }
   → String
 -- | I'm not able to use the fact from the constraint that
@@ -41,13 +41,28 @@ stringify'
 -- | Of course I can provide my dummy body or pass this record to the FFI
 stringify' r = unsafeStringify r
 
+-- | Here I can use required fields inside the body because we are stating them directly
+-- | in the type of the input record.
+stringify''
+  ∷ ∀ optionalMissing optionalPresent
+  . Union optionalPresent optionalMissing (OptionalFields + ())
+  ⇒ { | RequiredFields + optionalPresent }
+  → String
+stringify'' r = "req2:" <> r.req2 <> "; record: " <> unsafeStringify r
+
+
 logOptionalAndRequired ∷ Effect Unit
 logOptionalAndRequired = do
   log $ stringify' { req1: 8.0, req2: "test" }
+  log $ stringify' { opt2: 2, req1: 8.0, req2: "test" }
   log $ stringify' { opt1: true, opt2: 2, req1: 8.0, req2: "test" }
 
   -- | This won't compile because
   -- log $ stringify' { opt1: true, opt2: 2, req2: "test" }
+
+  log $ stringify'' { req1: 8.0, req2: "test" }
+  log $ stringify'' { opt2: 2, req1: 8.0, req2: "test" }
+  log $ stringify'' { opt1: true, opt2: 2, req1: 8.0, req2: "test" }
 
 
 main ∷ Effect Unit
